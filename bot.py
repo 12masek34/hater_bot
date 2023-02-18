19468e28785a3871017aeefa51c008eb17e0f266
dmitrijmartys <12masek34@mail.ru>
2023-02-18 15:09:14 +0300

    added checking messages for links

--- bot.py
+++ bot.py
@@ -2,10 +2,20 @@
 import logging
 import os
 import re
+from random import choice
+from time import sleep
 
 from dotenv import load_dotenv
 from telegram import ForceReply, Update
-from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
+
+from text import text_choice
 
 load_dotenv()
 logging.basicConfig(
@@ -32,15 +42,22 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Echo the user message."""
     URL_PATTERN = r'[A-Za-z0-9]+://[A-Za-z0-9%-_]+(/[A-Za-z0-9%-_])*(#|\\?)[A-Za-z0-9%-_&=]*'
-    if re.match(URL_PATTERN, update.message.text).group():
-        message = 'Это ссылка!'
-        await update.message.reply_text(message)
+    link = re.match(URL_PATTERN, update.message.text)
+    if link is not None:
+        message_id = update.message.message_id
+        chat_id = update.message.chat_id
+        message = choice(text_choice)
+        sleep(choice(range(1,6)))
+        await context.bot.send_message(
+            chat_id=chat_id,
+            reply_to_message_id=message_id,
+            text=message,
+        )
 
 
 def main() -> None:
     """Start the bot."""
     application = Application.builder().token(TOKEN).build()
-
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
