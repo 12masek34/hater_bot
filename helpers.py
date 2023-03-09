f611a7410c8633ef194ffe2ef38ccb4663a1aa83
dmitrijmartys <12masek34@mail.ru>
2023-03-09 20:52:28 +0300

    add

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 
 import asyncio
 import http
+import json
 
 import httpx
 
@@ -14,6 +15,9 @@ def create_hate_message():
     hate_message = ''
     response = httpx.get(url='https://evilinsult.com/generate_insult.php?lang=ru&type=json')
     if response.status_code == http.HTTPStatus.OK:
-        hate_message = response.json()['insult']
+        try:
+            hate_message = response.json()['insult']
+        except json.decoder.JSONDecodeError:
+            hate_message = 'JSONDecodeError\nНе удалось получить ругательное сообщение.'
 
     return hate_message
